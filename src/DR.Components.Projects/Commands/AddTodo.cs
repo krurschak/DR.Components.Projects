99ef01a38be58da43f2bc4fa2c443bb204e6618f
Krur Schak <krurschak@gmail.com>
2021-04-17 16:22:33 +0200

    Initial code commit

---
+++ src/DR.Components.Projects/Commands/AddTodo.cs
@@ -0,0 +1,20 @@
+﻿using DR.Packages.MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DR.Components.Todo.Commands
+{
+    public interface AddTodo
+        : ICommand
+    {
+        Guid Id { get; }
+        Guid ProjectId { get; }
+        Guid CreatorId { get; }
+        string Name { get; }
+        string Description { get; }
+        Guid? AssignedUserId { get; }
+        DateTime? DueDateUtc { get; }
+        short Priority { get; }
+    }
+}
